b6f557642f8fad80e9fd961ae7f16b5e4f168cbd
Sondre Jahrsengene <sondre.jahrsengene@communicate.no>
2020-01-31 23:04:42 +0100

    Add movement script to player sprite

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float Speed = 5;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.Translate(new Vector3(x: 0, y: 1) * Time.deltaTime * Speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(new Vector3(x: 1, y: 0) * Time.deltaTime * Speed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Translate(new Vector3(x: -1, y: 0) * Time.deltaTime * Speed);
+        }
+    }
+}
