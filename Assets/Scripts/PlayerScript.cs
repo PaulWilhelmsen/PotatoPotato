64da38f9210184283b58bf49241ed94daf34c797
Sondre Jahrsengene <sondre.jahrsengene@communicate.no>
2020-02-01 00:00:02 +0100

    Add more physics based player movement

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,33 +2,39 @@
 
 public class PlayerScript : MonoBehaviour
 {
-    public float Speed = 5;
+    //new public Rigidbody2D rigidbody;
+    public LayerMask groundLayers;
+    public float moveSpeed;
+    public float jumpPower;
+    float targetMoveSpeed;
+
+    bool grounded;
 
     void Start()
     {
-        
     }
 
     void Update()
     {
-        
+        // Checks if gameObjects overlaps ground layer
+        grounded = Physics2D.OverlapArea(
+            new Vector2(transform.position.x - 0.5f, transform.position.y - 0.5f),
+            new Vector2(transform.position.x + 0.5f, transform.position.y - 0.51f),
+            groundLayers);
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-        {
-            gameObject.transform.Translate(new Vector3(x: 0, y: 1) * Time.deltaTime * Speed);
-        }
+        var rigidbody = gameObject.GetComponent<Rigidbody2D>();
 
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            gameObject.transform.Translate(new Vector3(x: 1, y: 0) * Time.deltaTime * Speed);
-        }
+        // Gets movement speed and applies it
+        targetMoveSpeed = Mathf.Lerp(rigidbody.velocity.x, Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, Time.deltaTime * 10);
+        rigidbody.velocity = new Vector2(targetMoveSpeed, rigidbody.velocity.y);
 
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        // Jumps if not grounded
+        if(grounded && (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)))
         {
-            gameObject.transform.Translate(new Vector3(x: -1, y: 0) * Time.deltaTime * Speed);
+            rigidbody.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
         }
     }
 }
