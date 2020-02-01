7f181ad5314a2736249f16a8c1d307686a32c1eb
Sondre Jahrsengene <sondreja@online.no>
2020-02-01 14:10:49 +0100

    Add voice controlled jump

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,20 +7,22 @@ public class PlayerScript : MonoBehaviour
     public float moveSpeed;
     public float jumpPower;
     public float jumpCooldownInSeconds;
+    public float loudnessThreshold;
+    private SpriteRenderer mouth;
 
     AudioSource audioSource;
-    private float currentUpdateTime = 0f;
     public float updateStep = 0.1f;
     public int sampleDataLength = 1024;
-    private float clipLoudness;
-    private float[] clipSampleData;
 
-    float targetMoveSpeed;
-    float jumpTimeStamp;
+    float jumpTimestamp;
+    float voiceTimestamp;
 
     void Start()
     {
-        jumpTimeStamp = Time.time;
+        mouth = gameObject.GetComponentsInChildren<SpriteRenderer>().Where(r => r.name == "Mouth").Single();
+        jumpTimestamp = Time.time;
+        voiceTimestamp = Time.time;
+
         audioSource = GetComponent<AudioSource>();
         audioSource.clip = Microphone.Start(null, true, 10, 44100);
         audioSource.Play();
@@ -28,40 +30,53 @@ public class PlayerScript : MonoBehaviour
 
     void Update()
     {
-        // Audio Loudness
-        currentUpdateTime += Time.deltaTime;
-        if (currentUpdateTime >= updateStep)
-        {
-            currentUpdateTime = 0f;
-            float[] samples = new float[audioSource.clip.samples * audioSource.clip.channels];
-            audioSource.clip.GetData(samples, 0);
-
-            for (int i = 0; i < samples.Length; ++i)
-            {
-                samples[i] = samples[i] * 0.5f;
-                clipLoudness += Mathf.Abs(samples[i]);
-            }
-
-            clipLoudness /= sampleDataLength;
-        }
+        
     }
 
     void FixedUpdate()
     {
-        var rigidbody = gameObject.GetComponent<Rigidbody2D>();
-        var mouth = gameObject.GetComponentsInChildren<SpriteRenderer>().Where(r => r.name == "Mouth").Single();
-        var mouthDirection = mouth.transform.position - gameObject.transform.position;
-
-        // Horizontal movement speed (messes up air speed)
-        //targetMoveSpeed = Mathf.Lerp(rigidbody.velocity.x, Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, Time.deltaTime * 10);
-        //rigidbody.velocity = new Vector2(targetMoveSpeed, rigidbody.velocity.y);
-
-        if (Time.time >= jumpTimeStamp &&
-            (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)))
+        var loudness = 0f;
+        if(Time.time >= jumpTimestamp && Time.time >= voiceTimestamp)
         {
-            jumpTimeStamp = Time.time + jumpCooldownInSeconds;
-            //rigidbody.velocity = new Vector2(rigidbody.velocity.x, (1f * jumpPower));
-            rigidbody.AddForce(new Vector2(mouthDirection.x * -jumpPower * clipLoudness, mouthDirection.y * -jumpPower * clipLoudness), ForceMode2D.Impulse);
+            voiceTimestamp = Time.time + 0.2f;
+            loudness = GetVolume() * 1000f;
+            print(loudness);
         }
+
+        if ((Time.time >= jumpTimestamp && 
+            (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))|| loudness > loudnessThreshold))
+        {
+            var jumpMultiplier = 1f;
+            if(loudness > loudnessThreshold)
+            {
+                jumpMultiplier = loudness / loudnessThreshold;
+            }
+            jumpTimestamp = Time.time + jumpCooldownInSeconds;
+            audioSource.clip = Microphone.Start(null, true, 10, 44100);
+            Jump(jumpMultiplier);
+        }
+    }
+
+    void Jump(float jumpMultiplier)
+    {
+        var mouthDirection = mouth.transform.position - gameObject.transform.position;
+        var rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        var force = jumpPower * jumpMultiplier;
+        rigidbody.AddForce(new Vector2(mouthDirection.x * -force, mouthDirection.y * -force), ForceMode2D.Impulse);
+    }
+
+    float GetVolume()
+    {
+        float[] samples = new float[audioSource.clip.samples * audioSource.clip.channels];
+        audioSource.clip.GetData(samples, 0);
+
+        var clipLoudness = 0f;
+        for (int i = 0; i < samples.Length; ++i)
+        {
+            samples[i] = samples[i] * 0.5f;
+            clipLoudness += Mathf.Abs(samples[i]);
+        }
+
+        return clipLoudness /= audioSource.clip.samples * audioSource.clip.channels;
     }
 }
