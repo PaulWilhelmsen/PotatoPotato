efacae384f14033e54cd15eaceb614c0bcbd0b54
Paul <Paul.Henrik@gmail.com>
2020-02-01 14:20:50 +0100

    Working on joints

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums : MonoBehaviour
+{
+
+    public enum JointSpot
+    {
+        TopLeft,
+        DownLeft,
+        TopRight,
+        DownRight
+    }
+}
---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private Rigidbody2D _rb2d;
+    private HingeJoint2D _joint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+        _joint = GetComponent<HingeJoint2D>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"Hit with {other.gameObject.name} where tag is {other.gameObject.tag}");
+        if (other.gameObject.tag != "JointSpot") return;
+
+        SetNewPosition(other.transform.position);
+        ConnectJoint(other.gameObject);
+    }
+
+    private void SetNewPosition(Vector3 newPosition)
+    {
+        transform.position = newPosition;
+    }
+
+    private void ConnectJoint(GameObject connectTo)
+    {
+        _joint.connectedBody = connectTo.GetComponent<Rigidbody2D>();
+    }
+}
