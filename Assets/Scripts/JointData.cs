22624c7f6e30c864e57f3ea3d7d9bf9aaf936b08
Paul <Paul.Henrik@gmail.com>
2020-02-01 16:51:49 +0100

    Joints kinda work

--- Assets/Scripts/JointData.cs
+++ Assets/Scripts/JointData.cs
@@ -9,6 +9,7 @@ public class JointData : MonoBehaviour
     public bool IsTaken { get; private set; }
     public float MaxLimit;
     public float MinLimit;
+    public float ZRotation;
 
     public JointAngleLimits2D Limits()
     {
