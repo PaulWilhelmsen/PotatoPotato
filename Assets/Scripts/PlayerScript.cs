f9b979dc6fd1ebf959eec7c1c8e9ee57241d20d4
Paul <Paul.Henrik@gmail.com>
2020-02-01 22:53:25 +0100

    Added neutral face

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,28 +1,16 @@
-﻿using System.Collections;
-using System.Linq;
+﻿using System.Linq;
 using UnityEngine;
 using UnityEngine.Assertions;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
-    public SpriteRenderer img;
     public LayerMask groundLayers;
-    public FaceScript Emotions;
-    public Text loseText;
-    public Text scoreText;
     public float moveSpeed;
     public float jumpPower;
     public float jumpCooldownInSeconds;
     public float loudnessThreshold;
     public float maxVelocity = 10;
     private SpriteRenderer mouth;
-    public int maxXReached;
-
-    private bool isMoving;
-    public float speed;
-    private bool hasTouchedGround = false;
 
     AudioSource audioSource;
     public float updateStep = 0.1f;
@@ -33,11 +21,6 @@ public class PlayerScript : MonoBehaviour
 
     void Start()
     {
-        Emotions = GetComponentInChildren<FaceScript>();
-        img.color = new Color(0, 0, 0, 0);
-        maxXReached = 0;
-        loseText.gameObject.SetActive(false);
-        isMoving = true;
         mouth = gameObject.GetComponentsInChildren<SpriteRenderer>().Where(r => r.name == "Mouth").Single();
         Assert.IsNotNull(mouth);
         jumpTimestamp = Time.time;
@@ -50,30 +33,11 @@ public class PlayerScript : MonoBehaviour
 
     void Update()
     {
-        var rb = GetComponent<Rigidbody2D>();
-        maxXReached = Mathf.Max((int)rb.position.x, maxXReached);
-        scoreText.text = $"Score: {maxXReached}";
-
-        speed = rb.velocity.magnitude;
-
-        if (!hasTouchedGround || !(Mathf.Abs(speed) > 0.018)) return;
         
-        rb.constraints = RigidbodyConstraints2D.FreezeAll;
-        loseText.gameObject.SetActive(true);
-        isMoving = false;
-        StartCoroutine(FadeImage());
     }
 
     void FixedUpdate()
     {
-        if (!isMoving)
-        {
-            if (Input.GetKey(KeyCode.R))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
-
         GetComponent<Rigidbody2D>().velocity = Vector2.ClampMagnitude(GetComponent<Rigidbody2D>().velocity, maxVelocity);
         var loudness = 0f;
         if(Time.time >= jumpTimestamp && Time.time >= voiceTimestamp)
@@ -81,6 +45,7 @@ public class PlayerScript : MonoBehaviour
             audioSource.mute = false;
             voiceTimestamp = Time.time + 0.2f;
             loudness = GetVolume() * 1000f;
+            // print(loudness);
         }
 
         if ((Time.time >= jumpTimestamp && 
@@ -118,25 +83,5 @@ public class PlayerScript : MonoBehaviour
         }
 
         return clipLoudness /= audioSource.clip.samples * audioSource.clip.channels;
-    }
-
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Ground")
-        {
-            
-            hasTouchedGround = true;
-        }
-    }
-
-    IEnumerator FadeImage()
-    {
-        for (float i = 0; i <= 1; i += Time.deltaTime)
-        {
-            // set color with i as alpha
-            img.color = new Color(0, 0, 0, i);
-            yield return null;
-        }
     }
 }
