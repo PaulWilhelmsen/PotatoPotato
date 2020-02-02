585f4a3a0acead372631a8760cb109bbf8b0b50a
Paul <Paul.Henrik@gmail.com>
2020-02-02 12:55:14 +0100

    add particlesystem with leaves

---
+++ Assets/ParticleScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript : MonoBehaviour
+{
+    private ParticleSystem ps;
+    private PlayerScript _player;
+    public float hSliderValue = 1.0F;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        _player = FindObjectOfType<PlayerScript>();
+    }
+
+    void Update()
+    {
+        var main = ps.main;
+        main.simulationSpeed = hSliderValue + (_player.speed / 10);
+        
+    }
+}
